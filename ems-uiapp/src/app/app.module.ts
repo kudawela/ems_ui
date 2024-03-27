0e0518c3be101172716beb33d71be12c573b5d4e
Kalpani Madhushika <kalpaniudawela@gmail.com>
2024-03-27 18:37:28 +0900

    Install JSON server globally and create a service

--- ems-uiapp/src/app/app.module.ts
+++ ems-uiapp/src/app/app.module.ts
@@ -16,6 +16,7 @@ import {MatNativeDateModule} from '@angular/material/core'; // Added with datapi
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatNativeDateModule,
     MatRadioModule,
     MatSelectModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
