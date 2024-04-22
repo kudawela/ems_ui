56ad83de2c0322cef7688190a5e743a2289362f9
Kalpani Madhushika <kalpaniudawela@gmail.com>
2024-04-22 17:41:38 +0900

    UI for Table and view data

--- ems-uiapp/src/app/app.component.ts
+++ ems-uiapp/src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
 import { EmployeeService } from './services/employee.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,12 @@ import { EmployeeService } from './services/employee.service';
 })
 export class AppComponent implements OnInit{
   title = 'ems-uiapp';
+
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'dob', 'gender', 'education', 'company', 'experience', 'package'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     private _dialog: MatDialog, 
@@ -26,9 +35,20 @@ export class AppComponent implements OnInit{
   getEmployeeList(){
     this._empService.getEmployeeList().subscribe({
       next:(res) => {
-        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
       },
       error:console.log,
     })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
